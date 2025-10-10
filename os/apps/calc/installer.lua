6162d87a973b67bc8dfc1afb0e715ba60b833d1e
Kuschelcraft <108087587+Kuschelcraft@users.noreply.github.com>
2025-10-10 03:24:55 +0200

    fixed calc installer

--- os/apps/calc/installer.lua
+++ os/apps/calc/installer.lua
@@ -1,7 +1,7 @@
 local response = http.get("https://raw.githubusercontent.com/flakesystems/htos/refs/heads/main/os/apps/calc/updater.lua")
 if response then
     local code = response.readAll()
-    local file = fs.open("calc.lua", "w")
+    local file = fs.open("/os/apps/calc/updater.lua", "w")
     file.write(code)
     file.close()
 else
@@ -11,7 +11,7 @@ end
 local response = http.get("https://raw.githubusercontent.com/flakesystems/htos/refs/heads/main/os/apps/calc/uninstaller.lua")
 if response then
     local code = response.readAll()
-    local file = fs.open("calc.lua", "w")
+    local file = fs.open("/os/apps/calc/uninstaller.lua", "w")
     file.write(code)
     file.close()
 else
@@ -21,7 +21,7 @@ end
 local response = http.get("https://raw.githubusercontent.com/flakesystems/htos/refs/heads/main/os/apps/calc.json")
 if response then
     local code = response.readAll()
-    local file = fs.open("calc.lua", "w")
+    local file = fs.open("/os/apps/calc.json", "w")
     file.write(code)
     file.close()
 else
