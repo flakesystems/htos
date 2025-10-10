e3a2c3b991a8e921c4df0e8b4889d7e4cc8e3d35
Kuschelcraft <108087587+Kuschelcraft@users.noreply.github.com>
2025-10-10 03:29:44 +0200

    fixed calc installer again

--- os/apps/calc/installer.lua
+++ os/apps/calc/installer.lua
@@ -1,6 +1,9 @@
 local response = http.get("https://raw.githubusercontent.com/flakesystems/htos/refs/heads/main/os/apps/calc/updater.lua")
 if response then
     local code = response.readAll()
+    if fs.exists("/os/apps/calc/updater.lua") then
+        fs.delete("/os/apps/calc/updater.lua")
+    end
     local file = fs.open("/os/apps/calc/updater.lua", "w")
     file.write(code)
     file.close()
@@ -11,6 +14,9 @@ end
 local response = http.get("https://raw.githubusercontent.com/flakesystems/htos/refs/heads/main/os/apps/calc/uninstaller.lua")
 if response then
     local code = response.readAll()
+    if fs.exists("/os/apps/calc/uninstaller.lua") then
+        fs.delete("/os/apps/calc/uninstaller.lua")
+    end
     local file = fs.open("/os/apps/calc/uninstaller.lua", "w")
     file.write(code)
     file.close()
@@ -20,6 +26,9 @@ end
 
 local response = http.get("https://raw.githubusercontent.com/flakesystems/htos/refs/heads/main/os/apps/calc.json")
 if response then
+    if fs.exists("/os/apps/calc.json") then
+        fs.delete("/os/apps/calc.json")
+    end
     local code = response.readAll()
     local file = fs.open("/os/apps/calc.json", "w")
     file.write(code)
