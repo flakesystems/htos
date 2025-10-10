24640fd98471d7b114ec19c34ab8122d94aa26ab
Kuschelcraft <108087587+Kuschelcraft@users.noreply.github.com>
2025-10-10 03:26:19 +0200

    fix calc updater

--- os/apps/calc/updater.lua
+++ os/apps/calc/updater.lua
@@ -1,7 +1,7 @@
 local response = http.get("https://raw.githubusercontent.com/flakesystems/htos/refs/heads/main/os/apps/calc/calc.lua")
     if response then
         local code = response.readAll()
-        local file = fs.open("calc.lua", "w")
+        local file = fs.open("/os/apps/calc/calc.lua", "w")
         file.write(code)
         file.close()
     else
